026e2102c684753bfc20ccbe8f0ef63f3052c9b7
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-03-22 22:19:03 +1100

    fix: conflict files forget to resolve during 'git cherry-pick' session

--- cores/env.py
+++ cores/env.py
@@ -3,11 +3,7 @@ from pathlib import Path
 
 
 def get_env() -> dict[str, str | None]:
-<<<<<<< HEAD:cores/env.py
-    env_file: Path = (Path(__file__).resolve(strict=True).parent / "cfg.env")
-=======
     env_file = (Path(__file__).resolve(strict=True).parent / "cosmic_cfg.env")
->>>>>>> ba1a3fe (doc: update README to finalise last migration step to new repo):backend/cores/env.py
 
     return dotenv_values(
         dotenv_path=env_file,
