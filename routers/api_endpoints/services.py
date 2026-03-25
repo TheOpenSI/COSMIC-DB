d9c031684dfbbb0acd26d6cc3cc32748c5a68a80
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-03-25 21:07:38 +1100

    feat: add meaningful 500 error message when execution failed using Services API

--- routers/api_endpoints/services.py
+++ routers/api_endpoints/services.py
@@ -1,5 +1,9 @@
 ### Core modules ###
-from fastapi import APIRouter, HTTPException, status
+from fastapi import (
+    APIRouter,
+    HTTPException,
+    status
+)
 from sqlmodel import select
 
 
@@ -31,6 +35,9 @@ services_v1_router: APIRouter = APIRouter(
         },
         404: {
             "description": "Not Found (Services API V1)"
+        },
+        500: {
+            "description": "Internal Server Error (Services API V1)"
         }
     }
 )
@@ -44,9 +51,19 @@ services_v1_router: APIRouter = APIRouter(
 async def read_services_v1(
     session: SessionDependency
 ) -> Any:
-    services_view: Sequence[Services] = session.exec(statement=select(Services)).all()
+    try:
+        services_view: Sequence[Services] = session.exec(statement=select(Services)).all()
 
-    return services_view
+        return services_view
+
+    except Exception as fastapi_err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": "Internal Server Error",
+                "message": fastapi_err
+            }
+        )
 
 
 @services_v1_router.post(
@@ -58,13 +75,23 @@ async def create_service_v1(
     service: ServiceCreate,
     session: SessionDependency
 ) -> Any:
-    service_db: Services = Services.model_validate(obj=service, strict=True)
+    try:
+        service_db: Services = Services.model_validate(obj=service, strict=True)
 
-    session.add(instance=service_db)
-    session.commit()
-    session.refresh(instance=service_db)
+        session.add(instance=service_db)
+        session.commit()
+        session.refresh(instance=service_db)
 
-    return service_db
+        return service_db
+
+    except Exception as fastapi_err:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": "Internal Server Error",
+                "message": fastapi_err
+            }
+        )
 
 
 @services_v1_router.get(
@@ -76,15 +103,25 @@ async def read_service_v1(
     service_id: UUID,
     session: SessionDependency
 ) -> Any:
-    service_view: Services | None = session.get(entity=Services, ident=service_id)
+    try:
+        service_view: Services | None = session.get(entity=Services, ident=service_id)
 
-    if service_view is None:
+        if service_view is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Service Not Found!"
+            )
+        else:
+            return service_view
+
+    except Exception as fastapi_err:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Service Not Found!"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": "Internal Server Error",
+                "message": fastapi_err
+            }
         )
-    else:
-        return service_view
 
 
 @services_v1_router.patch(
@@ -97,22 +134,32 @@ async def update_service_v1(
     service: ServiceUpdate,
     session: SessionDependency
 ) -> Any:
-    service_db: Services | None = session.get(entity=Services, ident=service_id)
+    try:
+        service_db: Services | None = session.get(entity=Services, ident=service_id)
 
-    if service_db is None:
+        if service_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Service Not Found!"
+            )
+        else:
+            service_data: dict[str, Any] = service.model_dump(exclude_unset=True)
+            service_db.sqlmodel_update(obj=service_data)
+
+            session.add(instance=service_db)
+            session.commit()
+            session.refresh(instance=service_db)
+
+            return service_db
+
+    except Exception as fastapi_err:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Service Not Found!"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": "Internal Server Error",
+                "message": fastapi_err
+            }
         )
-    else:
-        service_data: dict[str, Any] = service.model_dump(exclude_unset=True)
-        service_db.sqlmodel_update(obj=service_data)
-
-        session.add(instance=service_db)
-        session.commit()
-        session.refresh(instance=service_db)
-
-        return service_db
 
 
 @services_v1_router.delete(
@@ -124,15 +171,25 @@ async def delete_service_v1(
     service_id: UUID,
     session: SessionDependency
 ) -> Any:
-    service_gone: Services | None = session.get(entity=Services, ident=service_id)
+    try:
+        service_gone: Services | None = session.get(entity=Services, ident=service_id)
 
-    if service_gone is None:
+        if service_gone is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Service Not Found!"
+            )
+        else:
+            session.delete(instance=service_gone)
+            session.commit()
+
+            return service_gone
+
+    except Exception as fastapi_err:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Service Not Found!"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "status": "Internal Server Error",
+                "message": fastapi_err
+            }
         )
-    else:
-        session.delete(instance=service_gone)
-        session.commit()
-
-        return service_gone
