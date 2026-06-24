460fe340e430fb85fd29835f4e82f933de9e242a
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-06-24 11:27:13 +1000

    chore: move `user_id` field up to top so we can see the relationship much quicker

--- apis/base_models.py
+++ apis/base_models.py
@@ -136,6 +136,13 @@ class ChatboxBase(SQLModel):
 
 
 class EmissionBase(SQLModel):
+    user_id: UUID = Field(
+        nullable=False,
+        sa_type=Uuid(
+            as_uuid=True,
+            native_uuid=True
+        ) # pyright: ignore
+    )
     run_id: UUID = Field(
         nullable=False,
         sa_type=Uuid(
@@ -291,11 +298,4 @@ class EmissionBase(SQLModel):
     wue: float | None = Field(
         default=None,
         nullable=True
-    )
-    user_id: UUID = Field(
-        nullable=False,
-        sa_type=Uuid(
-            as_uuid=True,
-            native_uuid=True
-        ) # pyright: ignore
     )
