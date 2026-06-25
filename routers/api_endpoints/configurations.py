a4cfce01c9f21b13d0ba8a90d934b44d98fc0103
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-06-25 20:00:55 +1000

    chore: no need to catch `ResponseValidationError` by ourself since it's done by `response_model` param

--- routers/api_endpoints/configurations.py
+++ routers/api_endpoints/configurations.py
@@ -170,16 +170,6 @@ async def create_config_v1(
             }
 
 
-    except ResponseValidationError as fastapi_exc:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            detail={
-                "status": "422 - Unprocessable Content",
-                "message": f"{fastapi_exc}"
-            }
-        )
-
-
     except TypeError as python_exc:
         # NOTE:
         # This one isn't likely to be catch that easy anymore since the only case
