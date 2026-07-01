40a2754e5d1debccc8a7553c7c8eecd456ee85ce
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-07-01 13:36:49 +1000

    feat: define common extra OpenAPI responses for single `GET` request in global file

--- cores/globals.py
+++ cores/globals.py
@@ -12,7 +12,7 @@ from typing import Any
 LLM_ROLE: str = 'assistant'
 
 
-OPENAI_UPDATE_EXTRA_RESPONSES: dict[int | str, dict[str, Any]] = {
+OPENAI_GET_EXTRA_RESPONSES: dict[int | str, dict[str, Any]] = {
     404: {
         "description": "Requested Data Not Found",
         "content": {
@@ -25,7 +25,11 @@ OPENAI_UPDATE_EXTRA_RESPONSES: dict[int | str, dict[str, Any]] = {
                 }
             }
         }
-    },
+    }
+}
+
+OPENAI_UPDATE_EXTRA_RESPONSES: dict[int | str, dict[str, Any]] = {
+    **OPENAI_GET_EXTRA_RESPONSES,
     409: {
         "description": "Foreign Key Integrity Constraint Error",
         "content": {
