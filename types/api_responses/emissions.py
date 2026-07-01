81fa4c6c6e835ec55e1d524c68bc2b176bbe7e99
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-07-01 12:38:56 +1000

    chore: update Response Model for Emissions API to use Pydantic's BaseModel directly instead of SQLModel

--- types/api_responses/emissions.py
+++ types/api_responses/emissions.py
@@ -1,5 +1,8 @@
 ### Core modules ###
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    ConfigDict
+)
 
 
 ### Type hints ###
@@ -17,16 +20,22 @@ from ...apis.data_models.emissions import (
 Client responses format according to FE requirements.
 """
 class EmissionsPublicResponse(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
     success:    bool
     count:      int
     result:     list[EmissionPublic]
 
 
 class EmissionCreateResponse(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
     success:    bool
     created:    EmissionPublic
 
 
 class EmissionDeleteResponse(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
     success:    bool
     deleted:    EmissionDelete
