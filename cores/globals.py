e397d21f138e8b5140241788c4e53ff4fc73b75f
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-07-01 13:33:05 +1000

    feat: define common extra OpenAPI responses for `PATCH` request in global file

--- cores/globals.py
+++ cores/globals.py
@@ -1,1 +1,42 @@
+### Core modules ###
+
+
+### Type hints ###
+from typing import Any
+
+
+### Internal modules ###
+
+
+
 LLM_ROLE: str = 'assistant'
+
+
+OPENAI_UPDATE_EXTRA_RESPONSES: dict[int | str, dict[str, Any]] = {
+    404: {
+        "description": "Requested Data Not Found",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": {
+                        "status": "404 - Not Found",
+                        "message": "string"
+                    }
+                }
+            }
+        }
+    },
+    409: {
+        "description": "Foreign Key Integrity Constraint Error",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": {
+                        "status": "409 - Conflict",
+                        "message": "string"
+                    }
+                }
+            }
+        }
+    }
+}
